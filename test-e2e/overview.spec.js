2c93b88798d60f2f87d8ae35cab47f57045abc5d
Whitney Harvey <wmelrose01@gmail.com>
2016-02-22 14:24:38 -0800

    fix promise issues in e2e test

--- test-e2e/overview.spec.js
+++ test-e2e/overview.spec.js
@@ -1,30 +1,92 @@
-describe('Overview page', function() {
+describe('MyApp', function() {
 
-  describe('capsule thumbnails', function() {
+  var capsuleElement;
 
-    beforeAll( function() {
-      browser.get('/');
-      element.all(by.repeater('capsule in capsules')).count().then( (count) => {
-        this.count = count;
+  describe('Overview page (modify capsules)', function() {
+
+    describe('capsule thumbnails', function() {
+
+      beforeAll( function() {
+        browser.get('/');
+        element.all(by.repeater('capsule in capsules')).count().then( (count) => {
+          this.count = count;
+        });
+      });
+
+      it('should add a capsule', function() {
+        element(by.model('season')).sendKeys('Spring');
+        element(by.model('description')).sendKeys('My test capsule');
+        $('.addButton').click();
+
+        var capsules = element.all(by.repeater('capsule in capsules'));
+        expect(capsules.count()).toEqual(this.count + 1);
+        capsuleElement = capsules.first().element(by.binding('capsule.description'));
+        expect(capsuleElement.getText()).toEqual('My test capsule');
+      });
+
+    });
+
+  });
+
+  describe('Detail page', function() {
+
+    describe('clothing items', function() {
+
+      beforeAll( function() {
+        capsuleElement.click();
+        element.all(by.repeater('item in items')).count().then( count => {
+          this.count = count;
+        });
+      });
+
+      it('should add an item', function() {
+        element(by.model('new.url')).sendKeys('www.test.com');
+        element(by.model('new.image')).sendKeys('www.testimage.com');
+        element(by.model('new.description')).sendKeys('jeans');
+        element(by.model('new.type')).$('[value="shoes"]').click();
+        $$('.rating-symbol').first().click();
+
+        $('.addButton').click();
+
+        var items = element.all(by.repeater('item in items'));
+
+        expect(items.count()).toEqual(this.count + 1);
+        expect(items.last().element(by.binding('item.description')).getText()).toEqual('jeans');
+
+      });
+
+      it('should delete an item', function() {
+        var items = element.all(by.repeater('item in items'));
+        items.last().$('.deleteButton').click();
+        var updatedItems = element.all(by.repeater('item in items'));
+        expect(updatedItems.count()).toEqual(this.count);
       });
     });
 
-    it('should add a capsule', function() {
-      element(by.model('season')).sendKeys('Spring');
-      element(by.model('description')).sendKeys('My test capsule');
-      element(by.css('.addButton')).click();
+  });
 
-      var capsules = element.all(by.repeater('capsule in capsules'));
-      expect(capsules.count()).toEqual(this.count + 1);
-      expect(capsules.first().element(by.binding('capsule.description')).getText()).toEqual('My test capsule');
+  describe('deleting the capsule', function() {
+
+    beforeAll( function() {
+      browser.get('/');
     });
 
     it('should delete a capsule', function() {
-      var capsules = element.all(by.repeater('capsule in capsules'));
-      capsules.last().element(by.css('.deleteButton')).click();
-      capsules = element.all(by.repeater('capsule in capsules'));
-      expect(capsules.count()).toEqual(this.count);
-    });
-});
+        var capsules = element.all(by.repeater('capsule in capsules'));
+        var initialCount;
 
-});
+        capsules.count()
+        .then( function(count) {
+          initialCount = count;
+          return capsules.first().$('.deleteButton').click();
+        })
+        .then( () => {
+          return element.all(by.repeater('capsule in capsules')).count();
+        })
+        .then( endCount => {
+          expect( endCount ).toEqual(initialCount - 1);
+        });
+      });
+  });
+
+})
